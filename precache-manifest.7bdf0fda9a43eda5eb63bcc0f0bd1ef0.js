dedde9557684929a0ab861bda82da47804b8a938
cre8rec <juliantmayes@gmail.com>
2020-08-13 12:54:28 -0700

    Updates

---
+++ precache-manifest.7bdf0fda9a43eda5eb63bcc0f0bd1ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6c111013c087a539507d4a386a340a",
+    "url": "/todo-progress/index.html"
+  },
+  {
+    "revision": "70dbd2da0e90f41b2985",
+    "url": "/todo-progress/static/css/main.3fb2945b.chunk.css"
+  },
+  {
+    "revision": "2172f601a4316db57e61",
+    "url": "/todo-progress/static/js/2.8940e853.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-progress/static/js/2.8940e853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70dbd2da0e90f41b2985",
+    "url": "/todo-progress/static/js/main.ba032bfb.chunk.js"
+  },
+  {
+    "revision": "1302b4f2c5d16c8614fd",
+    "url": "/todo-progress/static/js/runtime-main.8d0253de.js"
+  }
+]);
